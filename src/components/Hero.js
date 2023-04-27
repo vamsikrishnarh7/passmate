c84b98413e25d73f5a35d9f8f803aa0494fd4dd3
Durga Vamsi Krishna <vamsikrishnaacpt@gmail.com>
2023-04-27 18:32:39 +0530

    add style for sm

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='w-[90%] mx-auto text-white flex flex-col items-center' >
       <h1 className='text-[55px] sm:text-[80px]'>Never use <br/><span className='bg-white text-black'>Forget Password</span> <br/>anymore</h1>
       <button className='rounded-lg border-2 border-gray-600 mt-2 px-3 py-1 drop-shadow-[3px_3px_35px_white]'>SignIn</button>
-      <p className='my-10'>Don't have account? Let's fix that <Link to="/signup"><button className='ml-5 rounded-lg border-b-2 border-b-gray-600 px-3'>SignUp</button></Link></p>
+      <p className='my-10'>Don't have account? Let's fix that <Link to="/signup"><button className='sm:ml-5 rounded-lg border-b-2 border-b-gray-600 px-3'>SignUp</button></Link></p>
       
       <div className='flex gap-5 items-center mb-6'>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,7 @@ const NavBar = () => {
         <div onClick={handleNav} className='block md:hidden'>
             {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}            
         </div>
-        <div className={!nav ? 'bg-gray-900 fixed left-0 top-0 w-[50%] h-full border-r-2 border-r-gray-900 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <div className={!nav ? 'bg-gray-900 fixed left-0 top-0 w-[50%] h-full border-r-2 border-r-gray-900 ease-in-out duration-500 z-[999]' : 'fixed left-[-100%]'}>
             <h1 className='text-3xl font-bold p-4'>PassMate</h1>    
             <ul className='pt-24 uppercase ml-3 text-bold'>
                 <li className='p-4 rounded-lg border-b-2 border-b-gray-600'>Home</li>
